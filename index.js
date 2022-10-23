8037c6d12db911dbaf632e8f6932217f392a3294
helio <paul61843@gmail.com>
2022-10-23 01:17:38 +0000

    test call api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fetch = require('node-fetch');
 const app = express();
 const port = process.env.PORT || 5656;
 
@@ -6,6 +7,34 @@ app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
+app.get("/find", async (req, res) => {
+    const memberData = await findMemberData('id');
+    res.json(memberData);
+});
+
 app.listen(port, () => {
   console.log(`app running on ${port}`);
 });
+
+async function findMemberData(id) {
+    const url = `https://data.mongodb-api.com/app/data-ybwtl/endpoint/data/v1/action/findOne`
+    const headers = {
+        "Content-Type": "application/json",
+        "Access-Control-Request-Headers": "*",
+        "api-key": "lW050ldNtvMHO0xqzumxyTscgoHviynOVDbsrdd0CC4xaSvJUsiY8a2CWRkehKRV",
+    }
+    const body = {
+        "collection":"member",
+        "database":"church",
+        "dataSource":"Cluster0",
+        "filter": { "name" : id}
+    }
+
+    const memberData = await fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(body)
+    })
+
+    return await memberData.json();
+}
