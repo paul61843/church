870f1b1296fafecd6e25fe222219d387f922c6b4
helio <paul61843@gmail.com>
2022-10-23 23:13:38 +0000

    更新

--- index.js
+++ index.js
@@ -4,48 +4,53 @@ const app = express();
 const port = process.env.PORT || 5656;
 const mongoose = require('mongoose');
 
+const dbConfig = {
+  databaseName: 'church'
+}
+
 mongoose
-    .connect('mongodb+srv://paul61843:paul61843@cluster0.pyhkqqg.mongodb.net/?retryWrites=true&w=majority')
+    .connect(`mongodb+srv://paul61843:paul61843@cluster0.pyhkqqg.mongodb.net/${dbConfig.databaseName}?retryWrites=true&w=majority`)
     .then(() => console.log('Database is connected.'))
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
-  const Church = mongoose.model('church', { name: String } )
+  const Church = mongoose.model('member', { name: String } )
   const kitty = new Church({ name: 'Zildjian' });
   kitty.save().then(() => console.log('meow'));
   res.send("hello world!");
 });
 
-// app.get("/find", async (req, res) => {
-//     console.log('find');
-//     const memberData = await findMemberData('id');
-//     console.log(memberData)
-//     res.json(memberData);
-// });
 
-// app.listen(port, () => {
-//   console.log(`app running on ${port}`);
-// });
+app.get("/find", async (req, res) => {
+    console.log('find');
+    const memberData = await findMemberData('id');
+    console.log(memberData)
+    res.json(memberData);
+});
 
-// async function findMemberData(id) {
-//     const url = `https://data.mongodb-api.com/app/data-ybwtl/endpoint/data/v1/action/findOne`
-//     const headers = {
-//         "Content-Type": "application/json",
-//         "Access-Control-Request-Headers": "*",
-//         "api-key": "lW050ldNtvMHO0xqzumxyTscgoHviynOVDbsrdd0CC4xaSvJUsiY8a2CWRkehKRV",
-//     }
-//     const body = {
-//         "collection":"member",
-//         "database":"church",
-//         "dataSource":"Cluster0",
-//         "filter": { "name" : id}
-//     }
+async function findMemberData(id) {
+    const url = `https://data.mongodb-api.com/app/data-ybwtl/endpoint/data/v1/action/findOne`
+    const headers = {
+        "Content-Type": "application/json",
+        "Access-Control-Request-Headers": "*",
+        "api-key": "lW050ldNtvMHO0xqzumxyTscgoHviynOVDbsrdd0CC4xaSvJUsiY8a2CWRkehKRV",
+    }
+    const body = {
+        "collection":"member",
+        "database":"church",
+        "dataSource":"Cluster0",
+        "filter": { "name" : id}
+    }
 
-//     const memberData = await fetch(url, {
-//         method: "POST",
-//         headers: headers,
-//         body: JSON.stringify(body)
-//     })
+    const memberData = await fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(body)
+    })
 
-//     return await memberData.json();
-// }
+    return await memberData.json();
+}
+
+app.listen(port, () => {
+  console.log(`app running on ${port}`);
+});
